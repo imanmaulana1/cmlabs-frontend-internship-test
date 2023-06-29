e7ad19307fce1abceeea8f5cb209f903ce0d7bac
Iman Maulana <maulanaiman902@gmail.com>
2023-06-30 03:28:34 +0700

    add responsive navbar

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,10 @@
 const endpoints = 'https://www.themealdb.com/api/json/v1/1/categories.php'
-let myData = []
+const drawerMenu = document.querySelector('.categories-wrapper')
+const openDrawer = document.querySelector('.nav-links')
+const openBar = document.querySelector('.nav-bars')
+const closeDrawer = document.querySelector('.close')
+const listCategories = document.querySelector('.categories-body')
+
 const getData = async () => {
   try {
     const { data } = await axios.get(endpoints)
@@ -9,9 +14,33 @@ const getData = async () => {
   }
 }
 
+const openDrawerMenu = () => {
+  drawerMenu.classList.add('show')
+  document.querySelector('.hero-cta').classList.remove('animation')
+}
+
+const closeDrawerMenu = () => {
+  drawerMenu.classList.remove('show')
+  document.querySelector('.hero-cta').classList.add('animation')
+}
+
 const main = async () => {
   const res = await getData()
   console.log(res)
+  res.forEach((item) => {
+    const liElement = document.createElement('li')
+    liElement.classList.add('categories-body-item')
+
+    const aElement = document.createElement('a')
+    aElement.innerText = item.strCategory
+
+    liElement.appendChild(aElement)
+    listCategories.appendChild(liElement)
+  })
+
+  openBar.addEventListener('click', openDrawerMenu)
+  openDrawer.addEventListener('click', openDrawerMenu)
+  closeDrawer.addEventListener('click', closeDrawerMenu)
 }
 
 main()
