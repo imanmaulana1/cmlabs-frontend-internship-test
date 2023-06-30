e9369aeb3e4eed7c1d304cc92803cc20031b3889
Iman Maulana <maulanaiman902@gmail.com>
2023-06-30 08:53:20 +0700

    add detail category page

--- dist/js/detail_category.js
+++ dist/js/detail_category.js
@@ -1,4 +1,11 @@
-import { BASE_URL } from './index.js'
+import { BASE_URL, getData } from './index.js'
+
+const breadcumb = document.querySelector('.detail-category-breadcumb')
+const categoryTitle = document.querySelector('.detail-category-title')
+const categoryDescription = document.querySelector(
+  '.detail-category-description',
+)
+const categoryGrid = document.querySelector('.detail-category-grid')
 
 const getDetailCategory = async (param) => {
   try {
@@ -8,3 +15,42 @@ const getDetailCategory = async (param) => {
     console.log(error)
   }
 }
+
+const init = async () => {
+  const breadcumbElement = document.createElement('a')
+  breadcumbElement.innerText = localStorage.getItem('categoryName')
+  breadcumb.appendChild(breadcumbElement)
+
+  categoryTitle.innerText = localStorage.getItem('categoryName')
+
+  const result = await getData()
+  result?.forEach((item) => {
+    if (item.strCategory === localStorage.getItem('categoryName')) {
+      categoryDescription.innerText = item.strCategoryDescription
+    } else {
+      return
+    }
+  })
+
+  const res = await getDetailCategory(localStorage.getItem('categoryName'))
+  res?.meals?.forEach((item) => {
+    const boxWrapper = document.createElement('div')
+    boxWrapper.classList.add('box-detail-food-wrapper')
+
+    const divElement = document.createElement('div')
+    divElement.classList.add('box-detail-food')
+    divElement.style.backgroundImage = `url('${item.strMealThumb}')`
+
+    const spanElement = document.createElement('span')
+    spanElement.classList.add('label-detail-food')
+    spanElement.innerText = item.strMeal
+
+    divElement.appendChild(spanElement)
+    boxWrapper.appendChild(divElement)
+    categoryGrid.appendChild(boxWrapper)
+  })
+  console.log(res)
+  console.log(result)
+}
+
+init()
--- dist/js/index.js
+++ dist/js/index.js
@@ -7,7 +7,7 @@ const closeDrawer = document.querySelector('.close')
 const listCategories = document.querySelector('.categories-body')
 const gridCategories = document.querySelector('.category-grid')
 
-const getData = async () => {
+export const getData = async () => {
   try {
     const { data } = await axios.get(`${BASE_URL}/categories.php`)
     return data.categories
@@ -19,35 +19,45 @@ const getData = async () => {
 const openDrawerMenu = () => {
   drawerMenu.classList.add('show')
   document.querySelector('.hero-cta').classList.remove('animation')
+  document.querySelector('body').style.overflow = 'hidden'
 }
 
 const closeDrawerMenu = () => {
   drawerMenu.classList.remove('show')
   document.querySelector('.hero-cta').classList.add('animation')
+  document.querySelector('body').style.overflow = 'auto'
 }
 
 const main = async () => {
   const res = await getData()
 
   res.forEach((item) => {
-    const divElement = document.createElement('div')
-    divElement.classList.add('box-food')
-    divElement.style.backgroundImage = `url('${item.strCategoryThumb}')`
-
-    const spanElement = document.createElement('span')
-    spanElement.classList.add('box-food-label')
-    spanElement.innerText = item.strCategory
-
     const liElement = document.createElement('li')
     liElement.classList.add('categories-body-item')
 
     const aElement = document.createElement('a')
     aElement.innerText = item.strCategory
+    aElement.href = './detail_category.html'
+    aElement.addEventListener('click', () => {
+      localStorage.setItem('categoryName', item.strCategory)
+    })
+
+    const divElement = document.createElement('div')
+    divElement.classList.add('box-food')
+    divElement.style.backgroundImage = `url('${item.strCategoryThumb}')`
+    divElement.addEventListener('click', () => {
+      localStorage.setItem('categoryName', item.strCategory)
+      window.location.href = './detail_category.html'
+    })
+
+    const spanElement = document.createElement('span')
+    spanElement.classList.add('box-food-label')
+    spanElement.innerText = item.strCategory
 
     liElement.appendChild(aElement)
     listCategories.appendChild(liElement)
     divElement.appendChild(spanElement)
-    gridCategories.appendChild(divElement)
+    gridCategories?.appendChild(divElement)
   })
 
   openBar.addEventListener('click', openDrawerMenu)
